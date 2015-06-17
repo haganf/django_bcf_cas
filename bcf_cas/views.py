e4ddd5ae622c606bdaffb537adb4c85c587ceb52
Nick Eddy <eddy.nicholas@gmail.com>
2015-06-17 09:58:45 -0700

    fix no reverse issue that has been plaguing /admin/

--- bcf_cas/views.py
+++ bcf_cas/views.py
@@ -4,14 +4,13 @@ from urllib import urlencode
 import urlparse
 from operator import itemgetter
 
-from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponseForbidden, HttpResponse
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
-from cas.models import PgtIOU
+from bcf_cas.models import PgtIOU
 from django.contrib import messages
 
-__all__ = ['login', 'logout', 'verify_token']
+__all__ = ['login', 'logout']
 
 
 def _service_url(request, redirect_to=None, gateway=False):
@@ -21,14 +20,14 @@ def _service_url(request, redirect_to=None, gateway=False):
     host = request.get_host()
     prefix = (('http://', 'https://')[request.is_secure()] + host)
     service = protocol + host + request.path
-    if redirect_to:
+    if redirect_to:        
         if '?' in service:
             service += '&'
         else:
             service += '?'
         if gateway:
             """ If gateway, capture params and reencode them before returning a url """
-            gateway_params = [(REDIRECT_FIELD_NAME, redirect_to), ('gatewayed', 'true')]
+            gateway_params = [ (REDIRECT_FIELD_NAME, redirect_to), ('gatewayed','true') ]
             query_dict = request.GET.copy()
             try:
                 del query_dict['ticket']
@@ -36,15 +35,15 @@ def _service_url(request, redirect_to=None, gateway=False):
                 pass
             query_list = query_dict.items()
 
-            # remove duplicate params
+            #remove duplicate params
             for item in query_list:
                 for index, item2 in enumerate(gateway_params):
                     if item[0] == item2[0]:
                         gateway_params.pop(index)
             extra_params = gateway_params + query_list
-
-            # Sort params by key name so they are always in the same order.
-            sorted_params = sorted(extra_params, key=itemgetter(0))
+            
+            #Sort params by key name so they are always in the same order.
+            sorted_params = sorted(extra_params, key=itemgetter(0))            
             service += urlencode(sorted_params)
         else:
             service += urlencode({REDIRECT_FIELD_NAME: redirect_to})
@@ -55,14 +54,14 @@ def _redirect_url(request):
     """Redirects to referring page, or CAS_REDIRECT_URL if no referrer is
     set.
     """
-
+    
     next = request.GET.get(REDIRECT_FIELD_NAME)
     if not next:
         if settings.CAS_IGNORE_REFERER:
             next = settings.CAS_REDIRECT_URL
         else:
             next = request.META.get('HTTP_REFERER', settings.CAS_REDIRECT_URL)
-
+                        
         host = request.get_host()
         prefix = (('http://', 'https://')[request.is_secure()] + host)
         if next.startswith(prefix):
@@ -72,8 +71,8 @@ def _redirect_url(request):
 
 def _login_url(service, ticket='ST', gateway=False):
     """Generates CAS login URL"""
-    LOGINS = {'ST': 'login',
-              'PT': 'proxyValidate'}
+    LOGINS = {'ST':'login',
+              'PT':'proxyValidate'}
     if gateway:
         params = {'service': service, 'gateway': 'true'}
     else:
@@ -82,7 +81,7 @@ def _login_url(service, ticket='ST', gateway=False):
         params.update(settings.CAS_EXTRA_LOGIN_PARAMS)
     if not ticket:
         ticket = 'ST'
-    login = LOGINS.get(ticket[:2], 'login')
+    login = LOGINS.get(ticket[:2],'login')
 
     return urlparse.urljoin(settings.CAS_SERVER_URL, login) + '?' + urlencode(params)
 
@@ -100,6 +99,7 @@ def _logout_url(request, next_page=None):
 
 def login(request, next_page=None, required=False, gateway=False):
     """Forwards to CAS login URL or verifies CAS ticket"""
+
     if not next_page:
         next_page = _redirect_url(request)
     if request.user.is_authenticated():
@@ -116,28 +116,26 @@ def login(request, next_page=None, required=False, gateway=False):
         user = auth.authenticate(ticket=ticket, service=service)
 
         if user is not None:
-            # don't allow non-netid users to come through here, nor non-validated email addresses.
-            if not user.is_netid:
-                return HttpResponse('That NetID is not valid.', status=409)
-            elif not user.email_verified:
-                return HttpResponse('You must verify your email address.', status=409)
-
-            # Has ticket, logs in fine
+            #Has ticket, logs in fine    
             auth.login(request, user)
             if settings.CAS_PROXY_CALLBACK:
                 proxy_callback(request)
-            # keep urls
+            #keep urls
             return HttpResponseRedirect(next_page)
         elif settings.CAS_RETRY_LOGIN or required:
-            # Has ticket,
+            #Has ticket, 
             if gateway:
                 return HttpResponseRedirect(_login_url(service, ticket, True))
             else:
                 return HttpResponseRedirect(_login_url(service, ticket, False))
         else:
-            # Has ticket, not session
-            # TODO redirect user to /#/register using a reverse('apps.core.views.home') + '#/register'
-            return HttpResponseRedirect(reverse('apps.core.views.home') + '#/register')
+            #Has ticket, not session
+            if getattr(settings, 'CAS_CUSTOM_FORBIDDEN'):
+                from django.core.urlresolvers import reverse
+                return HttpResponseRedirect(reverse(settings.CAS_CUSTOM_FORBIDDEN) + "?" + request.META['QUERY_STRING'])
+            else:
+                error = "<h1>Forbidden</h1><p>Login failed.</p>"
+                return HttpResponseForbidden(error)
     else:
         if gateway:
             return HttpResponseRedirect(_login_url(service, ticket, True))
@@ -149,25 +147,20 @@ def logout(request, next_page=None):
     """Redirects to CAS logout page"""
 
     from django.contrib.auth import logout
-    if hasattr(request.user, 'is_netid') and request.user.is_netid:
-        if not next_page:
-            next_page = _redirect_url(request)
-        if settings.CAS_LOGOUT_COMPLETELY:
-            logout(request)
-            return HttpResponseRedirect(_logout_url(request, next_page))
-        else:
-            logout(request)
-            return HttpResponseRedirect(next_page)
+    logout(request)
+    if not next_page:
+        next_page = _redirect_url(request)
+    if settings.CAS_LOGOUT_COMPLETELY:
+        return HttpResponseRedirect(_logout_url(request, next_page))
     else:
-        logout(request)
-        return HttpResponseRedirect(reverse('home'))
+        return HttpResponseRedirect(next_page)
 
 
 def proxy_callback(request):
     """Handles CAS 2.0+ XML-based proxy callback call.
-    Stores the proxy granting ticket in the database for
+    Stores the proxy granting ticket in the database for 
     future use.
-
+    
     NB: Use created and set it in python in case database
     has issues with setting up the default timestamp value
     """
@@ -175,12 +168,10 @@ def proxy_callback(request):
     tgt = request.GET.get('pgtId')
 
     if not (pgtIou and tgt):
-        return HttpResponse('No pgtIOO', mimetype="text/plain")
+        return HttpResponse('No pgtIOO', content_type="text/plain")
     try:
         PgtIOU.objects.create(tgt=tgt, pgtIou=pgtIou, created=datetime.now())
         request.session['pgt-TICKET'] = ticket
-        return HttpResponse('PGT ticket is: %s' % str(ticket, mimetype="text/plain"))
+        return HttpResponse('PGT ticket is: %s' % str(ticket, content_type="text/plain"))
     except:
-        return HttpResponse('PGT storage failed for %s' % str(request.GET), mimetype="text/plain")
-
-    return HttpResponse('Success', mimetype="text/plain")
+        return HttpResponse('PGT storage failed for %s' % str(request.GET), content_type="text/plain")
